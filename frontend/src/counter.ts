1905b3b36892e79b69cb8038a91ad1abe7607f8b
GsuDev <156043286+GsuDev@users.noreply.github.com>
2025-11-10 22:05:59 +0100

    test: Commit de prueba

--- frontend/src/counter.ts
+++ frontend/src/counter.ts
@@ -5,8 +5,6 @@ export function setupCounter(element: HTMLButtonElement) {
         element.innerHTML = `count is ${counter}`
     }
 
-    element.addEventListener('click', () => {
-        setCounter(counter + 1)
-    })
+    element.addEventListener('click'      , () => setCounter(counter + 1))
     setCounter(0)
 }
