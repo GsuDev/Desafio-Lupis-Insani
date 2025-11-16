be1b0f61427bf0f5de4a145de8cf18e9f31ed887
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-11-16 23:27:45 +0100

    feat: Añade la migración create_participants_table

---
+++ backend/database/migrations/2025_11_16_222234_create_participants_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+           $table->id();
+            
+            $table->foreignId('game_id')
+                  ->constrained('games')
+                  ->onDelete('cascade');
+                  
+            $table->foreignId('user_id')
+                  ->nullable() // Para los botos
+                  ->constrained('users')
+                  ->onDelete('cascade');
+            
+            $table->boolean('is_bot')->default(false);
+            $table->string('bot_name')->nullable();
+            
+            $table->foreignId('character_id')
+                  ->constrained('characters')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+            
+            
+            //  estas validaciones las haré en el controlador
+            // pero las he puesto aqui como doble seguro
+            
+            // Un usuario no puede estar dos veces en la misma partida
+            $table->unique(['game_id', 'user_id']);
+            
+            // Un personaje no puede estar dos veces en la misma partida
+            $table->unique(['game_id', 'character_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants');
+    }
+};
