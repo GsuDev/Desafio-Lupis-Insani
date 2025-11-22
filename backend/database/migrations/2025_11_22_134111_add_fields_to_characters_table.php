67730261808ce13a787effcb7f3d4fbd2d494c83
GsuDev <156043286+GsuDev@users.noreply.github.com>
2025-11-22 22:00:54 +0100

    feat: Creo la migration de la tabla characters

---
+++ backend/database/migrations/2025_11_22_134111_add_fields_to_characters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->string('name')->after('id');
+
+            $table->boolean('is_unique')->default(true);
+
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->dropColumn([
+                'name',
+                'is_unique',
+                'description',
+            ]);
+        });
+    }
+};
