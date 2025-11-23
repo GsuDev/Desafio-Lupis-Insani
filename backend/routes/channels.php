2892d523b092c8542d56a91d0666ae3ecadde3ab
RoockDev <zar0_31@outlook.com>
2025-11-23 12:14:02 +0100

    feat: implementa autorización y validación de rol en canal de lobos

---
+++ backend/routes/channels.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+use App\Models\Participant;
+use App\Models\User;
+
+/*
+ * canal privado para los Lobos
+ * solopermite la entrada si el usuario es participante de la partida
+ * y si su personaje asignado es el ID 2 (Lobo)
+ */
+Broadcast::channel('wolves.{gameId}', function (User $user, int $gameId) {
+    
+    
+    
+    // detectamos el participante de si esta en la partida
+    $participant = Participant::where('user_id', $user->id)
+                            ->where('game_id', $gameId)
+                            ->first();
+
+    // Si no está en la partida, prohibido pasar false
+    if (!$participant) {
+        return false;
+    }
+
+   // se verifica   Rol
+    //  CharacterController: ID 2 = Lobo
+    // También aceptamos al personaje de la niña se implementara en el futuro
+    
+    return $participant->character_id === 2;
+});
