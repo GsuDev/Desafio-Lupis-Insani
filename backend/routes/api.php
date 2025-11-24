421c8bf0c262a85c58a6cfc6df9edfaf07809bbb
GsuDev <156043286+GsuDev@users.noreply.github.com>
2025-11-24 17:36:25 +0100

    Revert "feat: Middleware para comprobar acceso a canal"
    This reverts commit 6b747a64e3f84a77ebad2333068f65dde3d3a3e5.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\GameChannelController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -154,14 +153,5 @@ Route::middleware('auth:sanctum')->group(function () {
             //
         }
     );
-});
 
-/*
-|--------------------------------------------------------------------------
-| Rutas de los canales game
-|--------------------------------------------------------------------------
-*/
-// Route::middleware(['auth:sanctum', 'UserInGame'])->group(function () {
-//     Route::post('/games/{gameId}/events', [GameChannelController::class, 'send']);
-// });
-Route::post('/games/{gameId}/events', [GameChannelController::class, 'sendBridge']);
+});
