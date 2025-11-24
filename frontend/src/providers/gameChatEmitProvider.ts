9d34806ba3033d16d9f17756a52fa4a38f660a73
RoockDev <zar0_31@outlook.com>
2025-11-25 00:51:25 +0100

    feat: Añade GameChatEmitProvider para enviar mensajes a la API

---
+++ frontend/src/providers/gameChatEmitProvider.ts
@@ -0,0 +1,25 @@
+import apiClient from '../services/apiClient'
+import type { WolvesChatResponse } from '../interfaces/wolvesChatResponse'
+import type { ChatPayload } from '../interfaces/chatPayload'
+
+export class GameChatEmitProvider{
+    private gameId: number
+
+    constructor(gameId:number){
+        this.gameId = gameId
+    }
+
+    async emit(eventName: string, payload: ChatPayload):Promise<boolean>{
+        try{
+            const url = `/games/${this.gameId}/wolves/message`;
+            const { data } = await apiClient.post<WolvesChatResponse>(url, {
+                event: eventName,
+                data: payload
+            });
+            return data.success;
+        } catch (error) {
+            console.error('Error inesperado', error);
+            return false;
+        }
+}
+}
