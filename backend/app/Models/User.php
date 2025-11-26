e2fe83a02dde69ded4b56ab4f87cc9abeda929de
RoockDev <zar0_31@outlook.com>
2025-11-26 19:43:27 +0100

    feat: añade soporte anonimo al modelo user
    se añade 'is_anonymous' al fillable y se incluye en cast para asgurar que se trate como booleano en json

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
         'password',
         'birthdate',
         'profile_url',
+        'is_anonymous',
     ];
 
     protected $hidden = [
@@ -31,6 +32,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'birthdate' => 'date',
+        'is_anonymous' => 'boolean',
     ];
 
     /**
