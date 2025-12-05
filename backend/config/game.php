b1283fc0efb3d1262d390cd82223e29f97b7a2b5
RoockDev <zar0_31@outlook.com>
2025-12-05 23:11:28 +0100

    feat: Job transicion de noche

--- backend/config/game.php
+++ backend/config/game.php
@@ -4,5 +4,6 @@ return [
     'timers' => [
         // tiempo que tienen los jugadores para votar al alcalde en segundos
         'mayor_vote_duration' => env('GAME_MAYOR_VOTE_DURATION', 30),
+        'night_transition_duration' => env('GAME_NIGHT_TRANSITION_DURATION', 5),
     ],
 ];
