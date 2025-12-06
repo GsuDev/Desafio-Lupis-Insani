36a21eade393e914638d2facf1c59bbe6a9b7d8b
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2025-12-06 20:48:28 +0100

    feat: job de empezar las votaciones
    Se ha implementado el job de empezar las votaciones de los lobos

---
+++ backend/app/Jobs/StartWolvesVoteJob.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\GameEvent;
+use App\Events\WolvesEvent;
+use App\Models\Game;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class StartWolvesVoteJob implements ShouldQueue
+{
+    use Queueable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected int $gameId;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(int $gameId)
+    {
+        $this->gameId = $gameId;
+
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $game = Game::find($this->gameId);
+        if (! $game) {
+            return;
+        }
+
+        //Se tendría que controlar que el juego esté en la noche  ?
+
+        $duration = env('GAME_WOLVES_VOTE_DURATION', 10);
+
+        $text = "Lobos, es hora de acechar. Tenéis {$duration} segundos.";
+        $message = $game->addMessage('system', null, $text);
+
+        //envio al chat de los lobos que el evento a comenzado
+        broadcast(new WolvesEvent(
+            'chat.message',
+            ['message' => $message->toStructured()],
+            $this->gameId
+        ));
+
+        //emito el evento de cambio de fase
+        broadcast(new WolvesEvent(
+            'wolves.vote.start',
+            [
+                'phase' => 'night',
+                'gameId' => $this->gameId,
+            ],
+            $this->gameId
+        ));
+
+
+        //Ejemplo:
+        // ResolveWolvesVoteJob::dispatch($this->gameId)
+        //     ->delay(now()->addSeconds($duration)); // Aqui va el job que cambia de noche a día (game.day) y resuelve la votación (según tengo entendido).
+    }
+}
