f2cb16ae55153a9355c7f4eac2514380c5d53260
RoockDev <zar0_31@outlook.com>
2025-12-07 14:22:23 +0100

    feat: Conexion manager para mostrar pantalla fin de partida

--- frontend/src/managers/Game.manager.ts
+++ frontend/src/managers/Game.manager.ts
@@ -2,6 +2,8 @@ import { ChatController } from '../controllers/GameChatController'
 import { gameController } from '../controllers/GameController'
 import type { Message } from '../models/models'
 import type { ChatEvent, Event } from '../types/events.types'
+import { GameOverContainer } from '../components/gameOverContainer/gameOverContainer'
+import type { GameOverData } from '../components/gameOver/gameOverDetails'
 
 /**
  * En el manager se separan los eventos que vienen del router
@@ -10,7 +12,7 @@ import type { ChatEvent, Event } from '../types/events.types'
 export class GameManager {
     static handleEvent(
         eventName: string,
-        event: ChatEvent,
+        event: any /*ChatEvent*/,
         channel: 'game' | 'wolves'
     ): void {
         if (!event.data) {
@@ -26,6 +28,29 @@ export class GameManager {
                     event.data
                 )
                 break
+            //hu 45 pantalla game over
+            case 'game.conditions':
+                console.log('🏁 Evento de fin de partida recibido:', event.data);
+                
+                //  Verificamos si hay un ganador según contrato backend
+                // El backend envía winner: 'wolves' | 'villagers' | null
+                if (event.data.winner) {
+                    
+                    
+                    const rootNode = document.getElementById('app') || document.body;
+
+                    
+                    const gameOverData: GameOverData = event.data;
+
+                    
+                    const gameOverModal = new GameOverContainer(rootNode, gameOverData);
+                    
+                    
+                    gameOverModal.render();
+
+                    
+                }
+                break;
 
             case 'game.example':
                 // Cambiar para añadir
