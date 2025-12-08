d05ab2f09decc50c3d711f954af1d9db2087feec
RoockDev <zar0_31@outlook.com>
2025-12-08 20:46:46 +0100

    feat: panel administracion wip

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -176,6 +176,8 @@ class UserController extends Controller
             ], 404);
         }
 
+         $user->load('roles');
+
         return response()->json([
             'success' => true,
             'message' => 'Datos del usuario actual recuperados correctamente',
