be446327f522ffd3dd4e0c317dbf3421c73d391b
GsuDev <156043286+GsuDev@users.noreply.github.com>
2025-12-08 23:44:19 +0100

    fix: Users no hacian soft delete

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -27,6 +28,7 @@ class User extends Authenticatable
         'updated_at',
         'password',
         'remember_token',
+        'deleted_at',
     ];
 
     protected $casts = [
