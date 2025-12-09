b72424a53ab0827a08fb66bf750c45dc61c6f06c
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2025-12-09 02:01:25 +0100

    feat: time bar finalizada
    Carga las distintas fases del dia

--- frontend/src/managers/Game.manager.ts
+++ frontend/src/managers/Game.manager.ts
@@ -22,22 +22,31 @@ export class GameManager {
             console.warn(`⚠️ GameManager: datos vacíos para ${event}`)
             return
         }
-
         switch (eventName) {
             case 'game.day':
                 console.log('☀️ Evento de DÍA recibido:', event.data)
                 GameComponent.handleDayPhase(event.data)
                 GameComponent.handleVoteEnd // 🔥 Cerrar cualquier votación activa
+                GameComponent.onStateChange('DAY');
                 break
 
             case 'game.night':
                 console.log('🌙 Evento de NOCHE recibido:', event.data)
                 GameComponent.handleNightPhase(event.data)
                 GameComponent.handleVoteEnd // 🔥 Cerrar cualquier votación activa
+                GameComponent.onStateChange('NIGHT');
+
                 break
 
             case 'game.discussion':
                 // TODO: Sacar un enorme titulo para lobos o aldeanos
+                if(channel==='game'){
+                    GameComponent.onStateChange('DAY_DISCUSSION');
+                }else{
+                    GameComponent.onStateChange('NIGHT_DISCUSSION');
+                    
+                }
+                
                 console.log(
                     'Empieza la discusion: Cambiame por humo',
                     event.data
